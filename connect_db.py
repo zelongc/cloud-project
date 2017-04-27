486636ef252ac0eb44aec934c70d83d28a98a35f
zelongc <nickcong123@gmail.com>
2017-04-27 19:21:12 +1000

    Add files via upload

--- connect_db.py
+++ connect_db.py
@@ -1,7 +1,6 @@
-#!/usr/bin/python
+
 from couchdb import Server
-
-
+import datetime
 
 # server = Server() # connects to the local_server
 # >>> remote_server = Server('http://example.com:5984/')
@@ -10,13 +9,15 @@ from couchdb import Server
 class db_server(object):
 
     def __init__(self,username,login):
-        self.secure_server=Server('http://%s:%s@130.56.252.58:5984' %(username,login))
-        self.db=self.secure_server["tweet"]
-
+        self.secure_server = Server('http://%s:%s@115.146.95.12:5984' % (username, login))
+        self.db = self.secure_server["tweet"]
     def insert(self,data):
         try:
             doc_id,doc_rev=self.db.save(data)
         except Exception as e:
-            with open('log','a') as f:
+            with open('dabatase_log','a') as f:
+                f.write("["+datetime.datetime.now().__str__()+"]"+'\n')
                 f.write(str(e)+'\n')
                 f.write((data['_id']+'\n'))
+
+
